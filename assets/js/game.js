61f750cf15e75e15157360b39e1cc795a835470e
AmruthPillai <im.amruth@gmail.com>
2015-10-16 22:55:34 +0530

    updated game logic, added counters

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,9 +1,18 @@
+var stepCount = 1;
+var jumpResult = 0;
+
 $(".box").click(function() {
 	$(this).toggleClass("box-selected");
 });
 
 $(".box").click(function() {
+	$("#jumpResult").text( $(this).find("p").val() );
+});
+
+$(".box").click(function() {
 	$(".step-list .selected").toggleClass("selected").next("li").toggleClass("selected");
+	stepCount = stepCount + 1;
+	$("#stepCount").text(stepCount);
 });
 
 $(".box").click(function() {
