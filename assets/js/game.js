90b80fa0266326eb9efca1a59a36408068492d49
Amruth Pillai <im.amruth@gmail.com>
2015-10-17 00:02:09 -0700

    updated jumpResult calculation mechanism

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,20 +1,19 @@
 var stepCount = 1;
-var jumpResult = 0;
 
-$(".box").click(function() {
+$(".num-box").click(function() {
 	$(this).toggleClass("box-selected");
 });
 
-$(".box").click(function() {
-	$("#jumpResult").text( $(this).find("p").val() );
+$(".num-box").click(function() {
+	$("#jumpResult").append( parseInt( $(this).val() ) );
 });
 
-$(".box").click(function() {
+$(".num-box").click(function() {
 	$(".step-list .selected").toggleClass("selected").next("li").toggleClass("selected");
 	stepCount = stepCount + 1;
 	$("#stepCount").text(stepCount);
 });
 
-$(".box").click(function() {
+$(".num-box").click(function() {
 	$(".jump-list .selected").toggleClass("selected").prev("li").toggleClass("selected");
 });
