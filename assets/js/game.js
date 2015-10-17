401be86e2aacce8fdb732d99281c73af8eb5838d
Amruth Pillai <im.amruth@gmail.com>
2015-10-17 01:09:09 -0700

    updated game logic, breakthrough changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,20 @@
-var stepCount = 1;
-
+var jumpResult = 0;
 $(".num-box").click(function() {
 	$(this).toggleClass("box-selected");
-	
+	jumpResult = jumpResult + parseInt( $(this).text() );
+
 	if ( $(".box-selected").length == 2 ) {
+
+		$(".step-list .selected").toggleClass("selected").next("li").toggleClass("selected");
+
+		$(this).children().text(jumpResult);
 		$(".box-selected").removeClass("box-selected");
 
-});
+		for (i = 0; i < jumpResult; i++)
+    			$(".jump-list .selected").toggleClass("selected").prev("li").toggleClass("selected");
 
-$(".num-box").click(function() {
-	$("#jumpResult").append( parseInt( $(this).val() ) );
-});
+		jumpResult = 0;
 
-$(".num-box").click(function() {
-	$(".step-list .selected").toggleClass("selected").next("li").toggleClass("selected");
-	stepCount = stepCount + 1;
-	$("#stepCount").text(stepCount);
-});
+	}
 
-$(".num-box").click(function() {
-	$(".jump-list .selected").toggleClass("selected").prev("li").toggleClass("selected");
 });
