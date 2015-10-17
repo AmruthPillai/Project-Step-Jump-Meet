0b1d3072743ceeb69d4442984fc19c10a2359bf6
AmruthPillai <im.amruth@gmail.com>
2015-10-17 23:48:05 +0530

    major update, first working version of the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,12 @@
+// Game Variables
+var stepCounter = 1;
+var jumpCounter = 26;
 var jumpResult = 0;
+var negativeFlag = 0;
 
 // What should happen when the document is loaded?
 $(document).ready(function() {
-	$("#game-over-overlay h1").center();
+	$(".page-overlay h1").center();
 });
 
 // What should happen when a numbered box is clicked?
@@ -11,14 +15,24 @@ $(".num-box").click(function() {
 	// Toggle box-selected class for that particular object
 	$(this).toggleClass("box-selected");
 
-	// Increment the value of jumpResult
-	jumpResult = jumpResult + parseInt( $(this).text() );
+	// If the negative box was selected, toggle negativeFlag to ON
+	if ( $(this).hasClass("negative") )
+		negativeFlag = 1
 
 	// Check if 2 boxes are selected
 	if ( $(".box-selected").length == 2 ) {
 
+		if (negativeFlag == 0) {
+			// Increment the value of jumpResult
+			jumpResult += parseInt( $(this).text() );
+		} else if (negativeFlag == 1) {
+			// Decrement the value of jumpResult
+			jumpResult -= parseInt( $(this).text() );
+		}
+
 		// Increment the Step Counter Selector
 		$(".step-list .selected").toggleClass("selected").next("li").toggleClass("selected");
+		stepCounter += 1;
 
 		// Change the value of new jumpResult in the box
 		$(this).children().text(jumpResult);
@@ -26,15 +40,39 @@ $(".num-box").click(function() {
 		// Remove all selections from the Grid
 		$(".box-selected").removeClass("box-selected");
 
+		// Check if the negativeFlag was turned on, then
 		// Increment the Jump Counter by jumpResult number of times
-		for (i = 0; i < jumpResult; i++)
+		if (negativeFlag == 0) {
+			for (i = 0; i < jumpResult; i++)
     			$(".jump-list .selected").toggleClass("selected").prev("li").toggleClass("selected");
+    	} else if (negativeFlag == 1) {
+    		for (i = jumpResult; i < 0; i++)
+    			$(".jump-list .selected").toggleClass("selected").next("li").toggleClass("selected");
+    	}
 
-    	// Reset jumpResult for the next operation
+    	// If number of steps crosses 26, then stop the game and display losing message overlay
+    	if ( stepCounter == 27 ) {
+			$(".game-lose").css("visibility", "visible").children().fadeTo(500, 1);
+			throw new Error("Something went badly wrong!");
+		}
+
+		// If both the counters meet, then display congratulatory message overlay
+		if ( $(".step-list .selected").text() == $(".jump-list .selected").text() ) {
+			$(".game-win").css("visibility", "visible").children().fadeTo(500, 1);
+			throw new Error("Something went badly wrong!");
+		}
+
+		// Reset jumpResult for the next operation
 		jumpResult = 0;
 
-		if ( $(".step-list .selected").text() == $(".jump-list .selected").text() )
-			$("#game-over-overlay").css("visibility", "visible").children().fadeTo(500, 1);
+		// negativeFlag must be reset to 0
+		negativeFlag = 0;
+
+	} else {
+
+		// Return value of selected box into jumpResult
+		jumpResult = parseInt( $(this).text() );
+
 	}
 
 });
